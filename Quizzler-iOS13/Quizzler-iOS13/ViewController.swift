ffc6055c17c88b0e29fdb7908ad1527559313061
Euimin Chung <jane3079@korea.ac.kr>
2021-08-16 22:39:03 +0900

    [swift] struct

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -16,10 +16,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        ["Is 4 + 2 equal to 6?", "True"],
-        ["Is 5 - 3 equal to 1?", "False"],
-        ["Is 6 - 3 less than 4?", "True"]
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
     ]
+
     
     var questionNumber = 0
     let questionMaxIndex = 2
@@ -35,7 +45,8 @@ class ViewController: UIViewController {
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[questionNumber][1]
+        let actualQuestion = quiz[questionNumber].text
+        let actualAnswer = quiz[questionNumber].answer
         if userAnswer == actualAnswer {
             print("Right!")
         } else {
@@ -52,7 +63,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-            questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
 
     }
     
