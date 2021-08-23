d3ebd7d43ad81cbb26d059d70d6385838c1295ed
Euimin Chung <jane3079@korea.ac.kr>
2021-08-24 05:45:07 +0900

    swift change view through clicking a button

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -30,9 +30,13 @@ class ViewController: UIViewController {
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = weightSlider.value
-        print(weight / (height * height))
-
+        let bmi = weight / (height * height)
+        print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
     }
-    
 }
 
