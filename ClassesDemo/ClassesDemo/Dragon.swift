c3df91627ac05bec6dc9d0732ae12b8c241d9f68
Euimin Chung <jane3079@korea.ac.kr>
2021-08-23 20:25:19 +0900

    swift classes vs. struct

--- ClassesDemo/ClassesDemo/Dragon.swift
+++ ClassesDemo/ClassesDemo/Dragon.swift
@@ -1,21 +1,19 @@
-//Dragon inherited from Enemy
-class Dragon: Enemy {
-    var wingSpan = 2
-    
-    func talk(speech: String) {
-        print("Says: \(speech)")
-    }
-    
-    //Dragon is flying instead of walking
-    override func move() {
-        print("Fly forwards.")
-    }
-    
-    //not replacing, but acting both attack from Enemy(super class) and Dragon
-    override func attack() {
-        super.attack()
-        print("Spits fire, does \(attackStrength) damage.")
-        print("Spits fire, does \(skeleton.attackStrength) damage.")
-
-    }
-}
+////Dragon inherited from Enemy
+//class Dragon: Enemy {
+//    var wingSpan = 2
+//    
+//    func talk(speech: String) {
+//        print("Says: \(speech)")
+//    }
+//    
+//    //Dragon is flying instead of walking
+//    override func move() {
+//        print("Fly forwards.")
+//    }
+//    
+//    //not replacing, but acting both attack from Enemy(super class) and Dragon
+//    override func attack() {
+//        super.attack()
+//        print("Spits fire, does \(attackStrength) damage.")
+//    }
+//}
--- ClassesDemo/ClassesDemo/Enemy.swift
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -1,6 +1,15 @@
-class Enemy {
+struct Enemy {
     var health = 100
     var attackStrength = 10
+    
+    init(health: Int, attackStrength: Int) {
+        self.health = health
+        self.attackStrength = attackStrength
+    }
+    
+    mutating func takeDamage(amount: Int) {
+        health = health - amount
+    }
     
     func move() {
         print("Walk forwards.")
