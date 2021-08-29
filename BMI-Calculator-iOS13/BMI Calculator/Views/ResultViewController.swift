9b60d72afd658c7d8889c2d5a948a36c924e7717
Euimin Chung <jane3079@korea.ac.kr>
2021-08-29 18:20:49 +0900

    swift second view

---
+++ BMI-Calculator-iOS13/BMI Calculator/Views/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Euimin Chung on 2021/08/29.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
