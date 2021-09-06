5e09ae030437383e44e46f7b7d542381490acfa5
Euimin Chung <jane3079@korea.ac.kr>
2021-09-06 17:27:57 +0900

    swift BMI Calculator: done

--- BMI-Calculator-iOS13/BMI Calculator/Views/CalculatorBrain.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Views/CalculatorBrain.swift
@@ -6,18 +6,35 @@
 //  Copyright © 2021 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 struct CalculatorBrain {
     //var bmi: Float = 0.0
-    var bmi: Float? = nil
+    var bmi: BMI?
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? "0.0")
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? "0.0")
         return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "Error - No advice to be delivered"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
     
     mutating func calculateBMI(height: Float, weight: Float) {
         //bmi in line 21 == bmi in line 12 (mutating)
-        bmi = weight / (height * height)
+        let bmiValue = weight / (height * height)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more cookies", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Stay fit", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat fewer cookies", color: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
+        }
     }
+    
+
 }
