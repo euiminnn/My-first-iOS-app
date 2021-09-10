67dc907c57792262c9a9e122c44355432f9a0ba7
Euimin Chung <jane3079@korea.ac.kr>
2021-09-10 22:23:06 +0900

    swift Clima: Protocols

--- Protocols Demo/Protocols Demo/main.swift
+++ Protocols Demo/Protocols Demo/main.swift
@@ -1,11 +1,53 @@
-//
-//  main.swift
-//  Protocols Demo
-//
-//  Created by Euimin Chung on 2021/09/10.
-//
+protocol CanFly {
+    func fly()
+}
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+//    func fly() {
+//        print("The bird flaps its wings.")
+//    }
+}
 
-import Foundation
 
-print("Hello, World!")
+class Eagle: Bird, CanFly {    //class can inherit from both class and protocol
+    func fly() {
+        print("The eagle flaps its wings.")
+    }
+    
+    func soar() {
+        print("The eagle glides in the air using air currents.")
+    }
+}
 
+class Penguin: Bird {    //only class can inherit class.
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct Airplane: CanFly {    //struct can inherit protocols.
+    func fly() {
+        print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {    //protocol as a datatype
+        flyingObject.fly()
+    }
+}
+
+
+
+let myEagle = Eagle()
+let myPenguin = Penguin()
+let myPlane = Airplane()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myPlane)
