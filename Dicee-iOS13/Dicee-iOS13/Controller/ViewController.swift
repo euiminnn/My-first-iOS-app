3a26efd2f1a50e4250ad51eebd032394556841d5
Euimin Chung <jane3079@korea.ac.kr>
2021-10-18 16:10:30 +0900

    dice app rename variables

--- Dicee-iOS13/Dicee-iOS13/Controller/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/Controller/ViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var dice_one_real: UIImageView!
-    @IBOutlet weak var dice_two: UIImageView!
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
 
     var diceBrain = DiceBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dice_one_real.image = #imageLiteral(resourceName: "DiceSix")
-        dice_two.image = #imageLiteral(resourceName: "DiceFive")
+        diceOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceTwo.image = #imageLiteral(resourceName: "DiceFive")
         ResultLabel.text = ""
     }
 
@@ -26,8 +26,8 @@ class ViewController: UIViewController {
     @IBAction func rollinBPressed(_ sender: UIButton) {
         diceBrain.scoreOfPlayerOne = Int.random(in: 0...5)
         diceBrain.scoreOfPlayerTwo = Int.random(in: 0...5)
-        dice_one_real.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][diceBrain.scoreOfPlayerOne]
-        dice_two.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][diceBrain.scoreOfPlayerTwo]
+        diceOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][diceBrain.scoreOfPlayerOne]
+        diceTwo.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][diceBrain.scoreOfPlayerTwo]
         ResultLabel.text = diceBrain.checkWinner()
     }
 }
