fc6e8c000d7a09f72017eb2404f3d785e7ad6786
Euimin Chung <jane3079@korea.ac.kr>
2022-01-04 20:17:38 +0900

    [FEAT] add custom cell named: PaidCell

---
+++ WIP/whopaid/whopaid/PaidCell.swift
@@ -0,0 +1,23 @@
+//
+//  PaidCell.swift
+//  whopaid
+//
+//  Created by Euimin Chung on 2022/01/04.
+//
+
+import UIKit
+
+class PaidCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
