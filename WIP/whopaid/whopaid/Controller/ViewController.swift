1091d350c491a137bc708d2e80d317d0b11037d4
Euimin Chung <jane3079@korea.ac.kr>
2022-01-14 23:30:01 +0900

    주석 제거

--- WIP/whopaid/whopaid/Controller/ViewController.swift
+++ WIP/whopaid/whopaid/Controller/ViewController.swift
@@ -98,54 +98,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
         loadTransactions()
     }
-    
-/*
-    private var aAmount: Int {
-        get {
-            guard let number = Int(aPaidLabel.text!) else {
-                fatalError("Cannot convert a display label text to a Int: A.")
-            }
-            return number
-        }
-        set {
-            aPaidLabel.text = String(newValue)
-        }
-    }
-    private var bAmount: Int {
-        get {
-            guard let number = Int(bPaidLabel.text!) else {
-                fatalError("Cannot convert a display label text to a Int: B.")
-            }
-            return number
-        }
-        set {
-            bPaidLabel.text = String(newValue)
-        }
-    }
- */
-    /*
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        
-
-        
-        
-        if (aAmount + bAmount) != nil {
-            totalLabel.text = "\(aAmount + bAmount)"
-        }
-        if aAmount > bAmount {
-            let diff = aAmount - bAmount
-            resultLabel.text = "A가 B에게 \(diff/2)원 보내주면 돼요!"
-        } else if aAmount < bAmount {
-            let diff = bAmount - aAmount
-            resultLabel.text = "B가 A에게 \(diff/2)원 보내주면 돼요!"
-        } else {
-            resultLabel.text = "정산할게 없어요!"
-        }
-
-    }
-    */
-
-
 }
 
 
